a12d0bbcd42efa001758c49c27175880a54d90e5
agalvis <galcorp@gmail.com>
2019-02-09 23:12:50 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './public/home/home.component';
+import {NotFoundComponent} from './public/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', component: HomeComponent},
+  /*{
+    path: 'private',
+    loadChildren:'./private/admin/admin.module#AdminModule',
+    data: { name: 'Private' },
+    canActivate: [ PrivateGuard ]
+  },
+  {path: 'subscribe', component: SubscribeComponent},*/
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material/menu';
+import { HomeComponent } from './public/home/home.component';
+import { NotFoundComponent } from './public/not-found/not-found.component';
+import {MatIconModule, MatToolbarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
